a44fed23c05165ec1193d4ded49a2ebccb579871
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-24 06:46:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,10 +7,13 @@ attr_accessor :name
     @name = name
   end
 
+  def songs
+    @songs
+  end
+
   def add_song
   song.artist = self
   end
-
 
   def add_song_by_name(name)
     song = Song.new(name)
