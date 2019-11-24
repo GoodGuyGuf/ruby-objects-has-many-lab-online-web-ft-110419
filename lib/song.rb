a0c08b32d94460b5cfb395407f5234a4a00d7a7f
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-24 07:00:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ attr_accessor :artist, :name
 
   def artist_name
     song_artist = self.artist.name
-    if !song_artist
-    nil
-  end
+    if song_artist == ""
+      return nil
+    end
   end
 
 end
