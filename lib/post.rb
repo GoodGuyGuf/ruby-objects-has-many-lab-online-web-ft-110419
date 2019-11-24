70d56de65ac5145927e5244266c67869f52dbbfa
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-24 07:38:13 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,7 +3,17 @@ class Post
 attr_accessor :author, :name
 @@all = []
 
-def initialize(name)
-  @name = name
-  @@all << self
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+
+
+
+
+
+
+
+
 end
